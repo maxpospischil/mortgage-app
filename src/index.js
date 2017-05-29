f77d0ccf7bc116c554760cc53994b787d22303ec
Max Pospischil <mpospischil@demystdata.com>
2017-05-29 11:01:17 -0400

    rent vs own calc

--- src/index.js
+++ src/index.js
@@ -25,33 +25,100 @@ function CalculateOwningAdvantageAtYear(props) {
     <div>
       <h3>Owning</h3>
         <ul>Total of monthly mortgage payments: {totalOfMortgagePaymentsAtYear(props)}</ul>
-        <ul><h4>Dead Money</h4></ul>
-        <ul><p>Total of taxes: {totalOfTaxesAtYear(props)}</p></ul>
-        <ul><p>Total of HOA: {totalOfHoaAtYear(props)}</p></ul>
-        <ul><p>Total of interest: </p></ul>
+        <ul><h4>Money Spent</h4></ul>
+        <ul><p>Total of taxes: {round(totalOfTaxesAtYear(props))}</p></ul>
+        <ul><p>Total of interest: {round(totalOfInterestAtYear(props))}</p></ul>
+        <ul><p>Total of principal payments: {round(principalAccumulatedAtLengthOfResidence(props))}</p></ul>
+        <ul><p>Total of HOA: {round(totalOfHoaAtYear(props))}</p></ul>
+        <ul><p>Down Payment: {round(props.downPayment)}</p></ul>
+        <ul><p>Total of money spent: {round(totalOfMoneySpentAtPeriodOwn(props))}</p></ul>
         <ul><h4>Assets/Benefits</h4></ul>
-        <ul><p>Total Tax Deductions: </p></ul>
-        <ul><p>Principal Accrued: </p></ul>
-        <ul><p>Appreciation: </p></ul>
-        <ul><h4>Net Dead Money: $</h4></ul>
+        <ul><p>Total of tax deductions: {round(totalOfTaxDeduction(props))}</p></ul>
+        <ul><p>Principal Payments Accumulated: {round(principalAccumulatedAtLengthOfResidence(props))}</p></ul>
+        <ul><p>Down Payment: {round(props.downPayment)}</p></ul>
+        <ul><p>Appreciation: {round(totalAppreciation(props))}</p></ul>
+        <ul><p>Total of Benefits: {round(totalOfBenefits(props))}</p></ul>
+        <ul><h4>Net Dead Money: ${round(netDeadMoneyOwn(props))}</h4></ul>
       <h3>Renting</h3>
         <ul><h4>Dead Money</h4></ul>
-        <ul><p>Total of rent: </p></ul>
-        <ul><p>Total of renter's insurance </p></ul>
-
+        <ul><p>Total of rent: {round(totalOfRentPaymentsAtYear(props))}</p></ul>
+        <ul><p>Total of renter's insurance: {round(totalOfRentersInsurancePaymentsAtYear(props))} </p></ul>
+        <ul><h4>Net Dead Money: ${round(netDeadMoneyRent(props))}</h4></ul>
     </div>
-    
+    {resultStatement(props)}
   </div>);
+}
+
+function resultStatement(props) {
+  const netDeadRent = netDeadMoneyRent(props)
+  const netDeadOwn = netDeadMoneyOwn(props)
+  var sentence = null
+  if (netDeadRent > netDeadOwn) {
+    sentence = "You'd be $" + round(netDeadRent - netDeadOwn) + " richer if you purchased!!"
+  } else {
+    sentence = "You'd be $" + round(netDeadOwn - netDeadRent) + " richer if you rented!!"
+  }
+  return(
+    <div>
+      <h2>Result</h2>
+      <p>{sentence}</p>
+    </div>
+  )
+}
+
+function netDeadMoneyRent(props) {
+  return totalOfRentPaymentsAtYear(props) - totalOfRentersInsurancePaymentsAtYear(props)
+}
+
+function netDeadMoneyOwn(props) {
+  return totalOfMoneySpentAtPeriodOwn(props) - totalOfBenefits(props)
+}
+
+function totalOfBenefits(props) {
+  return totalOfTaxDeduction(props) + principalAccumulatedAtLengthOfResidence(props) + totalAppreciation(props) + props.downPayment
+}
+
+function totalOfTaxDeduction(props) {
+  return ((props.federalTaxRate + props.stateTaxRate)/100 * totalTaxDeductibleAtYear(props))
+}
+
+function totalAppreciation(props) {
+  return (propertyValueIncreaseMultiplier(props) * props.propertyCost) - props.propertyCost
+}
+
+function totalTaxDeductibleAtYear(props) {
+  return totalOfInterestAtYear(props) + totalOfTaxesAtYear(props)
+}
+
+function totalOfMoneySpentAtPeriodOwn(props) {
+  return totalOfInterestAtYear(props) + totalOfHoaAtYear(props) + totalOfTaxesAtYear(props) + principalAccumulatedAtLengthOfResidence(props) + props.downPayment
+}
+
+function totalOfInterestAtYear(props) {
+  return totalOfMortgagePaymentsAtYear(props) - principalAccumulatedAtLengthOfResidence(props)
+}
+
+function principalAccumulatedAtLengthOfResidence(props) {
+  const initialLoanAmount = amountBorrowed(props)
+  return round(initialLoanAmount - principalOwedAtLengthOfResidence(props))
+}
+
+function principalOwedAtLengthOfResidence(props) {
+  const monthlyPayment = calculateMonthlyMortgagePaymentFromProps(props)
+  const initialLoanAmount = amountBorrowed(props)
+  const monthlyRate = monthlyInterestRate(props.aprPercent)
+  const periods = (12 * props.lengthOfResidence)
+  return (monthlyPayment + ((1 + monthlyRate)**periods)*(monthlyRate*initialLoanAmount - monthlyPayment))/monthlyRate
 }
 
 function totalOfTaxesAtYear(props) {
   const averareAnnualPropertyTax = (props.propertyTax + (props.propertyTax * inflationRateMultiplier(props)))/2
-  return round(averareAnnualPropertyTax * props.lengthOfResidence)
+  return averareAnnualPropertyTax * props.lengthOfResidence
 }
 
 function totalOfHoaAtYear(props) {
   const averageAnnualHoa = (props.hoa + (props.hoa * inflationRateMultiplier(props)))/2
-  return round(averageAnnualHoa * props.lengthOfResidence)
+  return round(averageAnnualHoa * props.lengthOfResidence * 12)
 }
 
 function inflationRateMultiplier(props) {
@@ -63,16 +130,17 @@ function propertyValueIncreaseMultiplier(props) {
 }
 
 function totalOfRentPaymentsAtYear(props) {
-  return calculateMonthlyRentPayment(props) * props.lengthOfResidence
+  const averageAnnualRent = ((props.monthlyRent + (props.monthlyRent * inflationRateMultiplier(props)))/2)*12
+  return averageAnnualRent * (props.lengthOfResidence)
+}
+
+function totalOfRentersInsurancePaymentsAtYear(props) {
+  const averageAnnualRentersInsurance = ((props.monthlyRentersInsurance + (props.monthlyRentersInsurance * inflationRateMultiplier(props)))/2)*12
+  return averageAnnualRentersInsurance * (props.lengthOfResidence)
 }
 
 function totalOfMortgagePaymentsAtYear(props) {
-  return round(calculateMonthlyMortgagePayment(
-    props.propertyCost, 
-    props.aprPercent, 
-    props.mortgageLength, 
-    props.downPayment
-  ) * (props.lengthOfResidence * 12))
+  return round(calculateMonthlyMortgagePaymentFromProps(props) * (props.lengthOfResidence * 12))
 }
 
 function totalOfOwningPaymentsAtYear(props) {
@@ -80,23 +148,34 @@ function totalOfOwningPaymentsAtYear(props) {
 }
 
 function calculateMonthlyRentPayment(props) {
-  return props.monthlyRent;
+  return props.monthlyRent + props.monthlyRentersInsurance;
 }
 
 function calculateMonthlyOwnPayment(props) {
-  return props.hoa + (props.propertyTax / 12) + calculateMonthlyMortgagePayment(
+  return props.hoa + (props.propertyTax / 12) + calculateMonthlyMortgagePaymentFromProps(props);
+}
+
+function calculateMonthlyMortgagePaymentFromProps(props) {
+  return calculateMonthlyMortgagePayment(
     props.propertyCost, 
     props.aprPercent, 
     props.mortgageLength, 
-    props.downPayment
+    props.downPayment,
+    amountBorrowed(props)
   );
 }
 
-function calculateMonthlyMortgagePayment(propertyCost, aprPercent, mortgageLength, downPayment) {
+function amountBorrowed(props) {
+  return props.propertyCost - props.downPayment
+}
+
+function monthlyInterestRate(aprPercent) {
+  return (aprPercent/100)/12
+}
+
+function calculateMonthlyMortgagePayment(propertyCost, aprPercent, mortgageLength, downPayment, amountBorrowed) {
   const paymentPeriods = mortgageLength * 12
-  const apr = aprPercent/100
-  const monthlyInterest = apr/12
-  const amountBorrowed = propertyCost - downPayment
+  const monthlyInterest = monthlyInterestRate(aprPercent)
   return calculatePayment(amountBorrowed, monthlyInterest, paymentPeriods)
 }
 
@@ -114,9 +193,12 @@ class Calculator extends React.Component {
     this.handleMortgageLengthChange = this.handleMortgageLengthChange.bind(this);
     this.handleDownPaymentChange = this.handleDownPaymentChange.bind(this);
     this.handleRentChange = this.handleRentChange.bind(this);
+    this.handleRentersInsuranceChange = this.handleRentersInsuranceChange.bind(this);
     this.handleLengthOfResidenceChange = this.handleLengthOfResidenceChange.bind(this);
     this.handlePropertyValueIncreaseRateChange = this.handlePropertyValueIncreaseRateChange.bind(this);
     this.handleInflationRateChange = this.handleInflationRateChange.bind(this);
+    this.handleFederalTaxRateChange = this.handleFederalTaxRateChange.bind(this);
+    this.handleStateTaxRateChange = this.handleStateTaxRateChange.bind(this);
 
     this.state = {
       propertyCost: 182000,
@@ -126,9 +208,12 @@ class Calculator extends React.Component {
       mortgageLength: 15,
       downPayment: 36400,
       monthlyRent: 1940,
+      monthlyRentersInsurance: 14,
       lengthOfResidence: 5,
       propertyValueIncreaseRate: 3,
-      inflationRate: 2.5
+      inflationRate: 2.5,
+      federalTaxRate: 28,
+      stateTaxRate: 6.75
     };
   }
 
@@ -160,6 +245,10 @@ class Calculator extends React.Component {
     this.setState({monthlyRent: e.target.value});
   }
 
+  handleRentersInsuranceChange(e) {
+    this.setState({monthlyRentersInsurance: e.target.value});
+  }
+
   handleLengthOfResidenceChange(e) {
     this.setState({lengthOfResidence: e.target.value});
   }
@@ -172,6 +261,14 @@ class Calculator extends React.Component {
     this.setState({inflationRate: e.target.value});
   }
 
+  handleFederalTaxRateChange(e) {
+    this.setState({federalTaxRate: e.target.value});
+  }
+
+  handleStateTaxRateChange(e) {
+    this.setState({stateTaxRate: e.target.value});
+  }
+
   render() {
 
     const propertyCost = this.state.propertyCost;
@@ -182,10 +279,13 @@ class Calculator extends React.Component {
     const downPayment = this.state.downPayment;
 
     const monthlyRent = this.state.monthlyRent;
+    const monthlyRentersInsurance = this.state.monthlyRentersInsurance;
 
     const lengthOfResidence = this.state.lengthOfResidence;
     const propertyValueIncreaseRate = this.state.propertyValueIncreaseRate;
     const inflationRate = this.state.inflationRate;
+    const federalTaxRate = this.state.federalTaxRate;
+    const stateTaxRate = this.state.stateTaxRate;
 
     return (
       <div>
@@ -229,8 +329,13 @@ class Calculator extends React.Component {
           <input
             value={monthlyRent}
             onChange={this.handleRentChange} />
+          <legend>Enter your monthly renters insurance:</legend>
+          <input
+            value={monthlyRentersInsurance}
+            onChange={this.handleRentersInsuranceChange} />
           <MonthlyPaymentRent
             monthlyRent = {parseFloat(monthlyRent)}
+            monthlyRentersInsurance = {parseFloat(monthlyRentersInsurance)}
           />
         </fieldset>
 
@@ -247,6 +352,14 @@ class Calculator extends React.Component {
           <input
             value={inflationRate}
             onChange={this.handleInflationRateChange} />
+          <legend>Enter federal tax rate (as %):</legend>
+          <input
+            value={federalTaxRate}
+            onChange={this.handleFederalTaxRateChange} />
+          <legend>Enter state tax rate (as %):</legend>
+          <input
+            value={stateTaxRate}
+            onChange={this.handleStateTaxRateChange} />
           <CalculateOwningAdvantageAtYear
             propertyCost = {parseFloat(propertyCost)}
             hoa = {parseFloat(hoa)}
@@ -255,9 +368,12 @@ class Calculator extends React.Component {
             mortgageLength = {parseFloat(mortgageLength)} 
             downPayment = {parseFloat(downPayment)}
             monthlyRent = {parseFloat(monthlyRent)}
+            monthlyRentersInsurance = {parseFloat(monthlyRentersInsurance)}
             lengthOfResidence = {parseFloat(lengthOfResidence)}
             propertyValueIncreaseRate = {parseFloat(propertyValueIncreaseRate)}
             inflationRate = {parseFloat(inflationRate)}
+            federalTaxRate = {parseFloat(federalTaxRate)}
+            stateTaxRate = {parseFloat(stateTaxRate)}
           />
         </fieldset>
       </div>
